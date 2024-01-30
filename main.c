1169f68594a9b1ef5e252326ca06fe659d0a9878
calves20 <157167670+calves20@users.noreply.github.com>
2024-01-29 17:58:44 -0800

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
 #include "employee.h"
-#include "heap.h"
 
-int main(int argc, char *argv[])
-{
-	//TODO
-	
-	return 0;
+#define MAX_EMPLOYEES 5
+
+void heapSort(Employee *A, int n);  // Assuming heapSort is defined in heap.c
+void printList(Employee *A, int n); // Assuming printList is defined in heap.c
+
+int main() {
+    Employee employees[MAX_EMPLOYEES];
+    int i;
+
+    // Filling the array with user input
+    for (i = 0; i < MAX_EMPLOYEES; i++) {
+        printf("Enter name for employee %d: ", i + 1);
+        scanf("%s", employees[i].name); // assuming names do not have spaces
+        printf("Enter salary for employee %d: ", i + 1);
+        scanf("%d", &employees[i].salary);
+    }
+
+    // Sorting the array
+    heapSort(employees, MAX_EMPLOYEES);
+
+    // Printing the sorted list
+    printList(employees, MAX_EMPLOYEES);
+
+    return 0;
 }
+
